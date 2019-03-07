697f44b8d55e7d0859f5a8258b57dc319d9e2b52
obrousseau <olivier@dashthis.com>
2019-03-06 22:27:11 -0500

    Second part - post endpoint beginning

--- CashThis/Controllers/CurrencyController.cs
+++ CashThis/Controllers/CurrencyController.cs
@@ -32,5 +32,11 @@ namespace CashThis.Controllers
 
             return resources;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] CurrencyConversionResource resource)
+        {
+            //step 9 - https://medium.freecodecamp.org/an-awesome-guide-on-how-to-build-restful-apis-with-asp-net-core-87b818123e28
+        }
     }
 }
---
+++ CashThis/Resources/CurrencyConversionResource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashThis.Resources
+{
+    public class CurrencyConversionResource
+    {
+        [Required]
+        [MaxLength(30)]
+        public string OriginCurrencyInThreeLetterFormat { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string DestinationCurrencyInThreeLetterFormat { get; set; }
+
+        [Required]
+        public decimal AmountToConvert { get; set; }
+    }
+}
